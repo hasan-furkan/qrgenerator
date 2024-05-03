3837245b42567e9cc2d5859c0f3cc8af59e4fcd7
hasan-furkan <hsnfrkn32@gmail.com>
2024-05-04 01:37:41 +0200

    added qrcode code and save db

--- api/qrcodegenerator/views.py
+++ api/qrcodegenerator/views.py
@@ -1,7 +1,11 @@
+import os
+from django.http import JsonResponse
 from user.models import Users
 from .models import QrCode
 from rest_framework import viewsets, permissions
 from .serializers import QrCodeSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 import qrcode
 
@@ -14,12 +18,44 @@ class QrCodeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
 
-    def user_generate_qr_codes(self, user_id):
-        # user_id = Users.objects.get(id=user_id)
-        # qr = qrcode.QRCode(
-        #     version=1,
-        #     box_size=10,
-        #     border=5
-        # )
-        # data = user_id.email
-        pass
+    def user_generate_qr_codes(self, user_id, url):
+        user = Users.objects.get(id=user_id)
+        first_name = user.first_name
+        last_name = user.last_name
+
+        directory_path = 'media/qrcodes'
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+
+        image = qrcode.make(url)
+        file_name = f'{first_name}_{last_name}.png'
+        file_path = f'media/qrcodes/{file_name}'
+        image.save(file_path)
+
+        qr_code = QrCode.objects.create(
+            name=f'{first_name}_{last_name}',
+            user=user,
+            qr_code=f'qrcodes/{file_name}'
+        )
+        qr_code.save()
+        return qr_code
+
+    @swagger_auto_schema(
+        operation_description="Create website url QR code",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'website_url': openapi.Schema(type=openapi.TYPE_STRING, description='Website URL'),
+            },
+            required=['']
+        ),
+        responses={201: openapi.Response(description='Success')}
+    )
+    def create(self, request, *args, **kwargs):
+        user_id = request.user.id
+        website_url = request.data.get('website_url')
+        if not website_url:
+            return JsonResponse({'error': 'Website URL is required'}, status=400)
+        qr_code = self.user_generate_qr_codes(user_id, website_url)
+        serializer = self.get_serializer(qr_code)
+        return JsonResponse({'data': serializer.data}, status=201)
--- api/qrgenerator/urls.py
+++ api/qrgenerator/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -50,4 +52,4 @@ urlpatterns = [
 
     path('api/users/', include('user.urls')),
     path('api/qr/', include('qrcodegenerator.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
